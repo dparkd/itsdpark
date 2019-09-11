e4699309ddfbf83867fd93ffde9a2ba836155b66
Daniel Park <itsdpark@fb.com>
2019-09-11 16:49:13 -0400

    message

---
+++ bibim_/bibimshop/script.js
@@ -0,0 +1,24 @@
+$( function() {
+  var $grid = $('.grid').isotope({
+    itemSelector: 'article'
+  });
+});
+
+// debounce so filtering doesn't happen every millisecond
+function debounce( fn, threshold ) {
+  var timeout;
+  return function debounced() {
+    if ( timeout ) {
+      clearTimeout( timeout );
+    }
+    function delayed() {
+      fn();
+      timeout = null;
+    }
+    timeout = setTimeout( delayed, threshold || 100 );
+  }
+}
+
+$(window).bind("load", function() {
+  $('#all').click();
+});
